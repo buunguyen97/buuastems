00dff7fd34b2217ec0c4b625e27e8e16fb4f79bb
Buu Nguyen <buulenguyen@gmail.com>
2023-03-22 00:37:26 +0700

    them combo select

--- src/app/pages/admin/myComponents/combo-select/combo-select.component.ts
+++ src/app/pages/admin/myComponents/combo-select/combo-select.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-combo-select',
+  selector: 'combo-select',
   templateUrl: './combo-select.component.html',
   styleUrls: ['./combo-select.component.scss']
 })
 export class ComboSelectComponent {
-
+  @Input() optionsl1 = '데이터가 없습니다.';
+  @Input() optionsl2 = '데이터가 없습니다.';
+  @Input() optionsl3 = '데이터가 없습니다.';
+  @Input() label = 'Select bõx';
 }
