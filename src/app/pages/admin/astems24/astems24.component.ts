6e6f03f486f841a63a1a230535974746189e679a
buunguyen97 <buulenguyen@gmail.com>
2023-04-13 17:02:14 +0700

    astems24

--- src/app/pages/admin/astems24/astems24.component.ts
+++ src/app/pages/admin/astems24/astems24.component.ts
@@ -1,5 +1,5 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {DxButtonComponent, DxDataGridComponent} from 'devextreme-angular';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {DxButtonComponent, DxDataGridComponent, DxPopupComponent} from 'devextreme-angular';
 import {DxFormComponent} from 'devextreme-angular/ui/form';
 import DataSource from 'devextreme/data/data_source';
 import ArrayStore from 'devextreme/data/array_store';
@@ -20,10 +20,98 @@ export class Astems24Component implements OnInit {
   @ViewChild('mainGrid', {static: false}) mainGrid: DxDataGridComponent;
   @ViewChild('subGrid', {static: false}) subGrid: DxDataGridComponent;
   @ViewChild('foldableBtn', {static: false}) foldableBtn: DxButtonComponent;
+  @ViewChild('popup', {static: false}) popup: DxPopupComponent;
+  @ViewChild('popupForm', {static: false}) popupForm: DxFormComponent;
+  @ViewChild('popupGrid', {static: false}) popupGrid: DxDataGridComponent;
 
 
+  // @ts-ignore
+  dataTest = [{
+    tenant: 1000,
+    rcvKey: '1',
+    uid: 1,
+    fromRcvSchDate: '20/10/2022',
+    toRcvSchDate: '20/10/2022',
+    fromReceiveDate: '20/10/2022',
+    toReceiveDate: '20/10/2022',
+    sts: '취소 된',
+    rcvTypecd: '유형 2',
+    supplierId: '공급처3',
+    warehouseId: '창고 A',
+    ownerId: '화주 C',
+    acceptKey: 2,
+    rcvSchDate: '2/10/2022',
+    receiveDate: '2/10/2022',
+    rcvSumItemCount: 222000,
+    rcvSumQty1: 222000,
+    actFlag: 1,
+    remarks: 'ABC123',
+    createdBy: 'Buu',
+    createdDatetime: '13/4/2023',
+    modifiedBy: 'Buu',
+    modifiedDatetime: '13/4/2023',
+  },
+    {
+      tenant: 1000,
+      rcvKey: '2',
+      uid: 2,
+      fromRcvSchDate: '20/10/2022',
+      toRcvSchDate: '20/10/2022',
+      fromReceiveDate: '20/10/2022',
+      toReceiveDate: '20/10/2022',
+      sts: '취소 된',
+      rcvTypecd: '유형 2',
+      supplierId: '공급처1',
+      warehouseId: '창고 D',
+      ownerId: '화주 D',
+      acceptKey: 2,
+      rcvSchDate: '2/10/2022',
+      receiveDate: '2/10/2022',
+      rcvSumItemCount: 222000,
+      rcvSumQty1: 222000,
+      actFlag: 1,
+      remarks: 'ABC123',
+      createdBy: 'Buu',
+      createdDatetime: '13/4/2023',
+      modifiedBy: 'Buu',
+      modifiedDatetime: '13/4/2023',
+    }
+  ];
+
+  popupDataTest = [{
+    uid: 1,
+    tenant: 1000,
+    itemId: 112,
+    unit: 31,
+    expectQty1: 30,
+    receivedQty1: 4,
+    adjustQty1: 5,
+    lot1: 'example_lot1',
+    lot2: 'example_lot2',
+    lot3: 'example_lot3',
+    lot4: 'example_lot4',
+    lot5: 'example_lot5',
+    lot6: 'example_lot6',
+    lot7: 'example_lot7',
+    lot8: 'example_lot8',
+    lot9: 'example_lot9',
+    lot10: 'example_lot10',
+    damageFlg: 'example_damageFlg',
+    noShippingFlg: 'example_noShippingFlg',
+    foreignCargoFlg: 'example_foreignCargoFlg',
+    customsReleaseFlg: 'example_customsReleaseFlg',
+    taxFlg: 'example_taxFlg',
+    whInDate: '2023-04-15',
+    mngDate: '2023-04-16',
+    isSerial: 'example_isSerial',
+    createdBy: 'Buu',
+    createdDatetime: '13/4/2023',
+    modifiedBy: 'Buu',
+    modifiedDatetime: '13/4/2023',
+  }];
   // Global
   G_TENANT: any;
+  pageInfo: any = this.utilService.getPageInfo();
 
   mainFormData = {};
 
@@ -34,15 +122,48 @@ export class Astems24Component implements OnInit {
   subEntityStore: ArrayStore;
   key = 'uid';
 
-  dsWarehouse = []; // 창고
+  dsWarehouse = [
+    {uid: '창고 A', display: '창고 A'},
+    {uid: '창고 B', display: '창고 B'},
+    {uid: '창고 C', display: '창고 C'},
+    {uid: '창고 D', display: '창고 D'},
+  ]; // 창고
   dsItemAdmin = []; // 품목관리사
   dsCompany = []; // 거래처코드
-  dsRcvStatus = []; // 입고상태
-  dsRcvType = []; // 입고타입
-  dsSupplier = []; // 공급처
+  dsSupplierCountrycd = []; // 거래처코드
+  dsSupplierPortcd = []; // 거래처코드
+  dsRcvStatus = [
+    {code: '받지 못했다', display: '받지 못했다'},
+    {code: '받았다', display: '받았다'},
+    {code: '취소 된', display: '취소 된'}
+  ];
+  dsRcvType = [
+    {code: '유형 1', display: '유형 1'},
+    {code: '유형 2', display: '유형 2'},
+    {code: '유형 3', display: '유형 3'}
+  ]; // 입고타입
+  dsSupplier = [
+    {display: '공급처1', code: '공급처1'},
+    {display: '공급처2', code: '공급처2'},
+    {display: '공급처3', code: '공급처3'},
+    // ...
+  ];
   dsUser = []; // 사용자
-  dsOwner = []; // 화주
+  dsOwner = [
+    {uid: '화주 A', display: '화주 A'},
+    {uid: '화주 B', display: '화주 B'},
+    {uid: '화주 C', display: '화주 C'},
+    {uid: '화주 D', display: '화주 D'},
+  ];
+  dsYN = [];
+  changes = [];
 
+
+  isNewPopup = true;
+  popupKey = 'uid';
+  GRID_STATE_KEY = 'mm_code';
+  saveStatePopup = this.gridUtil.fnGridSaveState(this.GRID_STATE_KEY + '_popup');
+  loadStatePopup = this.gridUtil.fnGridLoadState(this.GRID_STATE_KEY + '_popup');
 
   constructor(
     public utilService: CommonUtilService,
@@ -50,6 +171,20 @@ export class Astems24Component implements OnInit {
     private codeService: CommonCodeService,
     public gridUtil: GridUtilService) {
     this.G_TENANT = this.utilService.getTenant();
+    this.onSave = this.onSave.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onClose = this.onClose.bind(this);
+  }
+
+  initCode(): void {
+
+    this.codeService.getCode(this.G_TENANT, 'YN').subscribe(result => {
+      this.dsYN = result.data;
+    });
+
+    this.codeService.getUser(this.G_TENANT).subscribe(result => {
+      this.dsUser = result.data;
+    });
   }
 
   ngOnInit(): void {
@@ -73,12 +208,12 @@ export class Astems24Component implements OnInit {
       store: this.subEntityStore
     });
 
-    this.codeService.getCode(this.G_TENANT, 'RCVSTATUS').subscribe(result => {
-      this.dsRcvStatus = result.data;
-    });
-    this.codeService.getCode(this.G_TENANT, 'RCVTYPE').subscribe(result => {
-      this.dsRcvType = result.data;
-    });
+    // this.codeService.getCode(this.G_TENANT, 'RCVSTATUS').subscribe(result => {
+    //   this.dsRcvStatus = result.data;
+    // });
+    // this.codeService.getCode(this.G_TENANT, 'RCVTYPE').subscribe(result => {
+    //   this.dsRcvType = result.data;
+    // });
     this.codeService.getUser(this.G_TENANT).subscribe(result => {
       this.dsUser = result.data;
     });
@@ -105,7 +240,10 @@ export class Astems24Component implements OnInit {
     this.utilService.getFoldable(this.mainForm, this.foldableBtn);
     this.initForm();
     // this.utilService.fnAccordionExpandAll(this.acrdn);  // 아코디언 모두 펼치기
-    this.utilService.getGridHeight(this.subGrid, 30);
+    // this.utilService.getGridHeight(this.subGrid, 30);
+    this.utilService.getShowBookMark(
+      {tenant: this.G_TENANT, userId: this.utilService.getUserUid(), menuL2Id: this.pageInfo.menuL2Id}, this.bookmarkBtn
+    ).then();
 
   }
 
@@ -115,37 +253,43 @@ export class Astems24Component implements OnInit {
 
     if (data.isValid) {
       // const result = await this.service.get(this.mainFormData);
-      const result = await this.service.get(this.mainFormData);
-      if (!result.success) {
-        this.utilService.notify_error(result.msg);
-        return;
-      } else {
-        this.mainGrid.instance.cancelEditData();
-        this.utilService.notify_success('search success');
-
-        this.mainEntityStore = new ArrayStore(
-          {
-            data: result.data,
-            key: this.key
-          }
-        );
-        this.mainDataSource = new DataSource({
-          store: this.mainEntityStore
-        });
-        await this.mainGrid.instance.deselectAll();
-        this.mainGrid.focusedRowKey = null;
-        this.mainGrid.paging.pageIndex = 0;
-
-        // 횡스크롤 제거
-        this.gridUtil.fnScrollOption(this.mainGrid);
-      }
+      // const result = await this.service.get(this.mainFormData);
+      const result = this.dataTest;
+      this.mainEntityStore = new ArrayStore(
+        {
+          data: result,
+          key: this.key
+        }
+      );
+      this.mainDataSource = new DataSource({
+        store: this.mainEntityStore
+      });
+      // this.mainGrid.dataSource = result;
+      // if (!result.success) {
+      //   this.utilService.notify_error(result.msg);
+      //   return;
+      // } else {
+      //   this.mainGrid.instance.cancelEditData();
+      //   this.utilService.notify_success('search success');
+      //
+      //   this.mainEntityStore = new ArrayStore(
+      //     {
+      //       data: result.data,
+      //       key: this.key
+      //     }
+      //   );
+      //   this.mainDataSource = new DataSource({
+      //     store: this.mainEntityStore
+      //   });
+      //   await this.mainGrid.instance.deselectAll();
+      //
+      //   // 횡스크롤 제거
+      //   this.gridUtil.fnScrollOption(this.mainGrid);
+      // }
     }
   }
 
   // 적치지시
-  onRowDblClick(e): void {
-    console.log('success');
-  }
 
   async onReset(): Promise<void> {
     await this.mainForm.instance.resetValues();
@@ -160,11 +304,125 @@ export class Astems24Component implements OnInit {
     this.mainForm.instance.getEditor('ownerId').option('value', this.utilService.getCommonOwnerId());
     this.mainForm.instance.getEditor('warehouseId').option('value', this.utilService.getCommonWarehouseId());
     this.mainForm.instance.getEditor('supplierId').option('value', this.utilService.getCompanyId());
+    this.mainForm.instance.getEditor('ownerId').option('value', this.utilService.getCompanyId());
     this.mainForm.instance.getEditor('sts').option('value', '200');
     this.mainForm.instance.focus();
   }
 
   onNew(e): void {
-    console.log('add success');
+    this.popup.visible = true;
+    this.isNewPopup = true;
+    this.popup.title = ('Add');
+  }
+
+  onInitNewRow(e): void {
+    e.data.tenant = this.G_TENANT;
+  }
+
+  onFocusedCellChanging(e, grid): void {
+    this.setFocusRow(e.rowIndex, grid);
+  }
+
+  onHiddenPopup(e): void {
+    this.popupForm.formData = {};
+    this.popupForm.instance.repaint();
+    this.popupGrid.dataSource = this.utilService.setGridDataSource([], this.popupKey);
+    this.popupGrid.instance.cancelEditData();
+    this.popupGrid.focusedRowKey = null;
+    this.popupGrid.paging.pageIndex = 0;
+
+    this.onSearch().then();
+  }
+
+  onRowDblClick(e): void {
+    this.isNewPopup = false;
+    this.popup.visible = true;
+    this.popup.title = ('Edit');
+    console.log(this.popupForm.instance.getEditor('sts'));
+    this.popupForm.instance.getEditor('sts').option('value', e.sts);
+    this.popupForm.instance.getEditor('rcvTypecd').option('value', e.rcvTypecd);
+    this.popupForm.instance.getEditor('supplierId').option('supplierId', e.rcvTypecd);
+    this.popupForm.instance.getEditor('warehouseId').option('warehouseId', e.rcvTypecd);
+    this.popupGrid.dataSource = this.utilService.setGridDataSource(this.popupDataTest, this.popupKey);
+    this.onSearchPopup(e.data).then();
+  }
+
+  async onSearchPopup(eData): Promise<void> {
+    this.popupForm.formData = eData;
+    this.popupGrid.dataSource = this.utilService.setGridDataSource(eData, this.popupKey);
+    // const executionTxt = this.utilService.convert1('com.msg.searchDetail', '상세조회');
+    // const result = await this.service.sendPost(eData, 'findCodeCategoryFull');
+
+    // if (this.utilService.resultMsgCallback(result, executionTxt)) {
+    //   this.popupForm.formData = result.data;
+    // this.popupGrid.dataSource = this.utilService.setGridDataSource(result.data.codeList, this.popupKey);
+    //   this.popup.visible = true;
+    // }
+  }
+
+  async onSave(e): Promise<void> {
+    const popupValidate = this.popupForm.instance.validate();
+    const popupFormData = this.popupForm.formData;
+    // const detailList = this.collectGridData(this.changes);
+    const executionTxt = this.utilService.convert1('com.btn.save', '저장');
+
+    if (popupValidate.isValid) {
+      // let result;
+      popupFormData.tenant = this.G_TENANT;
+      // popupFormData.codeList = detailList;
+
+      if (this.isNewPopup) {
+        // result = await this.service.sendPost(popupFormData, 'saveRcvExpected');
+        console.log('Add');
+      } else {
+        // result = await this.service.sendPost(popupFormData, 'updateRcvExpected');
+        console.log('Update');
+      }
+
+      // if (this.utilService.resultMsgCallback(result, executionTxt)) {
+      this.popup.visible = false;
+      this.onSearch().then();
+      // }
+    }
+  }
+
+  async onDelete(e): Promise<void> {
+    const popupFormData = this.popupForm.formData;
+    const executionTxt = this.utilService.convert1('com.btn.del', '삭제');
+
+    if (await this.utilService.confirm(this.utilService.convert('com.confirm.execute', executionTxt))) {
+      // const result = await this.service.sendPost(popupFormData, 'deleteCodeCategory');
+
+      // if (this.utilService.resultMsgCallback(result, executionTxt)) {
+      //   this.popup.visible = false;
+      // }
+    }
+  }
+
+  onClose(e): void {
+    this.popup.visible = false;
+  }
+
+  addClick(): void {
+
+    this.popupGrid.instance.addRow().then(() => {
+        const rowIdx = this.popupGrid.instance.getRowIndexByKey(this.changes[this.changes.length - 1].key);
+        this.setFocusRow(rowIdx, this.popupGrid);
+      }
+    );
+  }
+
+  async deleteClick(): Promise<void> {
+    const focusedRowIdx = this.popupGrid.focusedRowIndex;
+
+    if (focusedRowIdx > -1) {
+      this.popupGrid.instance.deleteRow(focusedRowIdx);
+      this.popupGrid.instance.getDataSource().store().push([{type: 'remove', key: this.popupGrid.focusedRowKey}]);
+      this.setFocusRow(focusedRowIdx - 1, this.popupGrid);
+    }
+  }
+
+  setFocusRow(index, grid): void {
+    grid.focusedRowIndex = index;
   }
 }
--- src/app/pages/admin/astems24/astems24.service.ts
+++ src/app/pages/admin/astems24/astems24.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {APPCONSTANTS} from '../../../shared/constants/appconstants';
 import {JHttpService} from '../../../shared/services/jhttp.service';
 import {ApiResult} from '../../../shared/vo/api-result';
+import {UserVO} from '../../common/layout8/layout8.service';
+import {COMMONINITDATA} from '../../../shared/constants/commoninitdata';
 
 @Injectable({
   providedIn: 'root'
@@ -32,10 +34,17 @@ export class Astems24Service {
     }
   }
 
-
+  async sendPost(data: any, command?: string): Promise<ApiResult<saveVO>> {
+    const url = `${this.httpUrl}/${command}`;
+    try {
+      return await this.http.post<ApiResult<saveVO>>(url, data).toPromise();
+    } catch (e) {
+      return COMMONINITDATA.DEFAULT_POST_API_ERROR;
+    }
+  }
 }
 
-// tslint:disable-next-line:class-nam class-name
+// tslint:disable-next-line:class-name
 export interface searchVO {
   tenant: string;
   rcvKey: string;
@@ -48,12 +57,59 @@ export interface searchVO {
   supplierId: number;
   warehouseId: number;
   ownerId: number;
-
-
 }
+
 
 // tslint:disable-next-line:class-name
 export interface detailVO {
   uid: number;
 
+}
+
+// tslint:disable-next-line:class-name
+export interface saveVO {
+  tenant: string;
+  rcvKey: string;
+  acceptKey: string;
+  sts: string;
+  rcvTypecd: string;
+  rcvSchDate: Date;
+  receiveDate: Date;
+  warehouseId: number;
+  ownerId: number;
+  remarks: string;
+  supplierId: number;
+  supplierCountrycd: string;
+  supplierPortcd: string;
+  supplierZip: string;
+  supplierAddress1: string;
+  supplierAddress2: string;
+  refName: string;
+  supplierPhone: string;
+  rcvDetailList: [{
+    uid: number;
+    tenant: string;
+    itemId: number;
+    expectQty1: number;
+    receivedQty1: number;
+    adjustQty1: number;
+    lot1: string;
+    lot2: string;
+    lot3: string;
+    lot4: string;
+    lot5: string;
+    lot6: string;
+    lot7: string;
+    lot8: string;
+    lot9: string;
+    lot10: string;
+    damageFlg: string;
+    noShippingFlg: string;
+    foreignCargoFlg: string;
+    customsReleaseFlg: string;
+    taxFlg: string;
+    whInDate: Date;
+    mngDate: Date;
+    isSerial: string;
+  }];
 }
